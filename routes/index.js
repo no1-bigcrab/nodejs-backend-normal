bb11b7d91cc608ef651a132f63fc4acbc7daa373
no1-bigcrab <nhiepphongts@gmail.com>
2020-03-27 20:52:41 +0700

    mongoose CRUD

--- routes/index.js
+++ routes/index.js
@@ -1,182 +1,66 @@
 var express = require('express');
 var router = express.Router();
-
-const MongoClient = require('mongodb').MongoClient;
-var convetObject_data = require('mongodb').ObjectID;
-
-const assert = require('assert');
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'contact';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
-});
+var contactModel = require('../model/contact.js')
+var conact
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET add page. */
-router.get('/add', function(req, res, next) {
-  res.render('add', { title: 'Add new data.' });
-});
-
-/* post add page. */
-router.post('/add', function(req, res, next) {
-  // data
-  var data = {
-    "name" : req.body.name,
-    "phone" :req.body.phone
-  }
-
-  //insert
-
-  const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('nguoidung');
-    // Insert some documents
-    collection.insert(data, function(err, result) {
-      assert.equal(err, null);
-    });
-  }
-
-  MongoClient.connect(url, function(err, client) {
-    assert.equal(null, err);
-  
-    const db = client.db(dbName);
-  
-    insertDocuments(db, function() {
-      client.close();
-
-    });
-  });
-  res.redirect('/view');
-
-
-});
-// xem dữ liệu
-
+/* Trang view xem dữ liệu*/ 
 router.get('/view', function(req, res, next) {
-
-  const findDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('nguoidung');
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-      assert.equal(err, null);
-      console.log(docs)
-      callback(docs);
-    });
-  }
-
-  MongoClient.connect(url, function(err, client) {
-    assert.equal(null, err);
-    const db = client.db(dbName);
-      findDocuments(db, function( dataDb ) {
-        res.render('view-data', { title: 'View data.', data : dataDb });
-        //console.log(dataDb);
-        client.close();
-      });
-    });
-
+  contactModel.find({}, function(err, dataDb){
+    res.render('view-data', { title: 'Xem dữ liệu', data : dataDb});
+  })
+  
 });
 
-//Xoá dữ liệu
+// Trang Xoá dữ liệu
 router.get('/delete/:id', function(req, res, next) {
-  var id_delete = convetObject_data( req.params.id );
-  // thực hiện xoá
-  const removeDocument = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('nguoidung');
-    // Delete document where a is 3
-    collection.deleteOne({ _id : id_delete }, function(err, result) {
-      assert.equal(err, null);
-      console.log("Xoá thành công!!!");
-      callback(result);
-    });
-  }
-  // kết nối xoá
-  MongoClient.connect(url, function(err, client) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-  
-    const db = client.db(dbName);
-        removeDocument(db, function() {
-          client.close();
+  var id_delete = req.params.id;
 
-        });
-  });
+  contactModel.findByIdAndRemove(id_delete).exec();
   res.redirect('/view');
-
 
 });
 
-//sửa dữ liệu
+// Trang Xoá dữ liệu
 router.get('/edit/:id', function(req, res, next) {
-  var id_edit = convetObject_data( req.params.id );
-
-  // dựa vào id để sửa
-
-  const findDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('nguoidung');
-    // Find some documents
-    collection.find({ _id : id_edit }).toArray(function(err, docs) {
-      assert.equal(err, null);
-      console.log("Found the following records");
-      console.log(docs);
-      callback(docs);
-    });
-  }
-  MongoClient.connect(url, function(err, client) {
-    assert.equal(null, err);
-    const db = client.db(dbName);
-      findDocuments(db, function( dataDb ) {
-        res.render('edit-data', { title: 'Edit data.', data : dataDb });
-        //console.log(dataDb);
-        client.close();
-      });
-    });
-
+  var id_edit = req.params.id;
+  contactModel.find( { _id : id_edit }, function( err, dataDb ){
+    res.render( 'edit-data', { title: 'Sửa dữ liệu', data : dataDb })
+  })
 });
 
-// lấy form sửa
 router.post('/edit/:id', function(req, res, next) {
-  var id_edit = convetObject_data( req.params.id );
-  var data = {
-    "name" : req.body.name,
-    "phone" :req.body.phone
-  }
-  const updateDocument = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('nguoidung');
-    // Update document where a is 2, set b equal to 1
-    collection.updateOne({ _id : id_edit }
-      , { $set: data }, function(err, result) {
-      assert.equal(err, null);
-    });
-  }
-  MongoClient.connect(url, function(err, client) {
-    assert.equal(null, err);  
-    const db = client.db(dbName);
-  
-      updateDocument(db, function() {
-        client.close();
-      });
+  var id_edit = req.params.id;
+  contactModel.findById( id_edit, function( err, data ){
+    if( err ) return handleError(err);
+    data.name = req.body.name;
+    data.tuoi = req.body.tuoi;
+    data.phone = req.body.phone;
+    data.save();
+    res.redirect('/view');
   });
+});
+
+// them du lieu.accordion
+
+// Trang thêm dữ liệu
+router.get('/add', function(req, res, next) {
+  res.render('add', { title: 'Thêm dữ liệu'});
+});
+router.post('/add', function(req, res, next) {
+  var data = {
+    'name' : req.body.name,
+    'tuoi' : req.body.tuoi,
+    'phone' : req.body.phone
+  }
+  var  AddData = new contactModel(data);
+  AddData.save();
   res.redirect('/view');
 
-  //res.render('add', { title: 'Add new data.' });
 });
+
 module.exports = router;
