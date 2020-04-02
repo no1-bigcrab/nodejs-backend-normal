a84b66533f8d8c75a5103a8799bc6732d794ae4b
no1-bigcrab <nhiepphongts@gmail.com>
2020-04-02 17:37:43 +0700

    đổ dữ liệu từ db

--- model/contact.js
+++ model/contact.js
@@ -1,5 +1,13 @@
 var mongoose = require('mongoose');
 
-var contact = new mongoose.Schema({ name: 'string', tuoi: 'number', phone: 'number' }, {collection: 'contact'});
+var contact = new mongoose.Schema({ 
+    name: { type : String },
+    image: { type : Array },
+    price: { type : Number }, 
+    short_title: { type : String }, 
+    description: { type : String } 
+}, {
+    collection: 'data'
+});
 
-module.exports = mongoose.model('contact', contact);
+module.exports = mongoose.model('data', contact);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
+var uploadModel = require('../model/contact.js')
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/testdb', {useNewUrlParser: true});
 
 var image_data_push = [];
 
@@ -22,9 +26,37 @@ router.post('/upload', upload.any(), function(req, res, next) {
   var images_name = req.files[0].path;
   
   image_data_push.push(images_name);
-  console.log(image_data_push);
+  //console.log(image_data_push);
   res.status(200).send(res.files);
 
   //res.render('index', { title: 'Express' });
+});
+
+router.post('/upProduct', function(req, res, next) {
+
+  var name = req.body.name,
+      price = req.body.price,
+      short_title = req.body.short_title,
+      description = req.body.description;
+
+  var oneObject = {
+      "name" : name,
+      "price" : price,
+      "image" : image_data_push,
+      "short_title" : short_title,
+      "description" : description
+  }
+
+  var data = new uploadModel(oneObject);
+  data.save();
+
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/view', function(req, res, next) {
+  uploadModel.find({}, function( error , data_product ){
+    // console.log( error, data_product );
+    res.render('view-data', { title: 'View data object', data: data_product });
+  })
 });
 module.exports = router;
